7e30ffec230319c9bc89fcccee39ea17b4192d4e
Charlie Kim <charlie2@ualberta.ca>
2024-08-07 14:17:52 -0600

    hello rectangle

--- src/main.c
+++ src/main.c
@@ -1,15 +1,22 @@
 #include "include/glad/glad.h"
 #include "include/glfw/glfw3.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+char *glslToBuffer(char *filename);
+void compileStatusCheck(GLuint shader);
+void programStatusCheck(GLuint program);
 
 int main() {
+  // GLFW init
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  // Window creation
   GLFWwindow *window = glfwCreateWindow(800, 600, "Hello world", NULL, NULL);
   if (window == NULL) {
     printf("Failed to create GLFW window");
@@ -17,19 +24,101 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  // GLAD init
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     printf("Failed to instantiate GLAD");
     return -1;
   }
 
-  glViewport(0, 0, 800, 600);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  // Compile shader
+  char *vertexShaderSource = glslToBuffer("./src/shaders/basic.glsl");
 
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  compileStatusCheck(vertexShader);
+
+  // Fragment shader
+  char *fragmentShaderSource = glslToBuffer("./src/shaders/basic_frag.glsl");
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  compileStatusCheck(fragmentShader);
+
+  // Shader program
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glUseProgram(shaderProgram);
+  programStatusCheck(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // Rectangle
+  float vertices[] = {
+      0.5f,  0.5f,  0.0f, // top right
+      0.5f,  -0.5f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f  // top left
+  };
+  unsigned int indices[] = {
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
+
+  // Bind vertexes to VBO, indices to EBO, all to VAO
+  unsigned int VBO, EBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  glBindVertexArray(0);
+
+  // Draw loop
   while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  // Deallocate resources
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+  glDeleteProgram(shaderProgram);
+
+  free(vertexShaderSource);
+  free(fragmentShaderSource);
 
   glfwTerminate();
   return 0;
@@ -37,4 +126,54 @@ int main() {
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, 1);
+  }
+}
+
+char *glslToBuffer(char *filename) {
+  char *buf;
+  long length;
+  FILE *f;
+
+  fopen_s(&f, filename, "rb");
+
+  if (f) {
+    fseek(f, 0, SEEK_END);
+    length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    buf = malloc(length + 1);
+    if (buf) {
+      fread(buf, 1, length, f);
+    }
+
+    fclose(f);
+    buf[length] = '\0';
+  }
+  return buf;
+}
+
+void compileStatusCheck(GLuint shader) {
+  int success;
+  char infoLog[512];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+  if (!success) {
+    glGetShaderInfoLog(shader, 512, NULL, infoLog);
+    printf("Shader compilation failed: \n%s", infoLog);
+  }
+}
+
+void programStatusCheck(GLuint program) {
+  int success;
+  char infoLog[512];
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+  if (!success) {
+    glGetProgramInfoLog(program, 512, NULL, infoLog);
+    printf("Program compilation failed: \n%s", infoLog);
+  }
 }
